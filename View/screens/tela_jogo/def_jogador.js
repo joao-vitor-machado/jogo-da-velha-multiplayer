b029a18ad328b74d84050cb5a796d954b95a3a9c
Ana Beatriz Barducco <barduccoana@gmail.com>
2021-10-11 10:44:26 -0300

    teste

--- View/screens/tela_jogo/def_jogador.js
+++ View/screens/tela_jogo/def_jogador.js
@@ -1,7 +1,3 @@
-//TESTE
-
-// para o html: <script type="text/javascript" src="js/def_jogador.js"></script>
-
 src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
 
 $(document).onload(function(){
@@ -9,3 +5,35 @@ $(document).onload(function(){
 });
 
 <div id="div1" style="width:80px;height:80px;background-color:red;"></div>
+
+/* figma:
+
+bolinha:
+height: 231px;
+width: 228px;
+left: 614px;
+top: 396px;
+
+X constraint: Left
+Y constraint: Top
+
+border: 8px solid #F406F9
+
+Align: Inside
+Width: 8px
+
+bordas:
+height: 518px;
+width: 1454px;
+left: -14px;
+top: 266px;
+
+border: 6px solid #F98906
+
+X constraint: Left
+Y constraint: Top
+
+Align: Center
+Width: 6px
+
+*/
