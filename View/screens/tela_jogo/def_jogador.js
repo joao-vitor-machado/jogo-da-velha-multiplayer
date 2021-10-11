e3e9546ad2b3235c45c924e03eb16f3f1cb09434
Ana Beatriz Barducco <barduccoana@gmail.com>
2021-10-11 10:15:17 -0300

    Create def_jogador.js

---
+++ View/screens/tela_jogo/def_jogador.js
@@ -0,0 +1,11 @@
+//TESTE
+
+// para o html: <script type="text/javascript" src="js/def_jogador.js"></script>
+
+src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+
+$(document).onload(function(){
+    $("#div1").fadeToggle();
+});
+
+<div id="div1" style="width:80px;height:80px;background-color:red;"></div>
