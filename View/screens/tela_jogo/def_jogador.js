0addb120dac7976523125e8289d8782047b2ab35
joao-vitor-machado <jv.muca@gmail.com>
2021-10-11 21:04:49 -0300

    criação da animação

--- View/screens/tela_jogo/def_jogador.js
+++ View/screens/tela_jogo/def_jogador.js
@@ -25,19 +25,45 @@ function mostrarPopup(elementoJogador){
     switch (elementoJogador) {
         case 1:
             imgJogador.src = "../../shared/imgs/jogadorO.png";
+            imgJogador.style = "width:100vw;";
             break;
         case 2:
             imgJogador.src = "../../shared/imgs/jogadorX.png";
+            imgJogador.style = "width:100vw;";
             break;
     }
 
-    //Inserindo todos os elementos ao popup
+    
+    //Adicionando imagem ao slide
     divImagemJogador.appendChild(imgJogador);
-    div.appendChild(divImagemJogador);
 
-    //Adicionando o pop-up ao body
+    //Animando a imagem
+    imgJogador.animate([
+        //keyframes
+      
+      { transform: 'translateX(100%)' },
+       
+       { transform: 'translateX(0%)' },
+       
+       {transform: 'translateX(-100%)'}
+     ], {
+        //timing options
+       duration: 6000,
+       fill: "both",
+     });
+   
+    //Adicionando a imagem e sua div ao body-img
+    div.appendChild(divImagemJogador);
+    
+    //Adicionando o slide ao body
     body.appendChild(div);
 
+    //Adicionando tudo ao body do html
     document.querySelector("body").appendChild(body);
+    
+    //Retirando o slide do html para acessar o jogo
+    setTimeout(function(){
+        document.querySelector("body").removeChild(body);
+    }, 6500);
 
 }
