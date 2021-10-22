236166fcc64b02befcbf5e053e0e0906ea8ecc23
joao-vitor-machado <jv.muca@gmail.com>
2021-10-21 21:47:34 -0300

    broadcast funcionando

--- python-websocket/client.js
+++ python-websocket/client.js
@@ -1,15 +1,18 @@
 
 const WebSocket = require('ws');
 
-const ws = new WebSocket("ws://localhost:7890");
+const client = new WebSocket("ws://localhost:7890");
 
-ws.addEventListener("open", e => {
-    ws.send(
+client.addEventListener("open", e => {
+    client.send(
         JSON.stringify({
                      linha: "linha",
                      coluna: "coluna"
                  })
     );
 
-
+    client.onmessage = function(event){
+        msgm = event.data
+        console.log(msgm);
+    }
 })
